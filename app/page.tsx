6cd488fbc10417833a56b9bd04ab490ec1526dfd
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-12-03 10:41:48 +0600

    yarn, mode-toggle added

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
+export default function Home() {
+  return (
+    <main>
+        <Button>default button</Button>
+        <ModeToggle/>
+    </main>
+      );
+}
